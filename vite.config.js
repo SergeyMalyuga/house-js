d0c6e91294395cb86d788e138f9fdb0199cc29bf
Sergey Malyuga <sergey@malyuga.ru>
2026-03-04 10:07:43 +0300

    feat(style.scss): написать базовые стили проекта

--- vite.config.js
+++ vite.config.js
@@ -1,51 +1,51 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import htmlInclude from "vite-plugin-html-include";
 
 export default defineConfig({
-    root: 'src',
-    publicDir: 'public',
+  root: "src",
+  publicDir: "../public",
 
-    server: {
-        port: 3000,
-        host: true,
-        open: true,
-    },
+  server: {
+    port: 3000,
+    open: true,
+    host: true,
+  },
 
-    build: {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourceMap: true,
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
 
-      rollupOptions: {
-        output: {
-          chunkFileNames: "js/[name].[hash].js",
-          entryFileName: "js/[name].[hash].js",
+    rollupOptions: {
+      output: {
+        chunkFileNames: "js/[name].[hash].js",
+        entryFileNames: "js/[name].[hash].js",
 
-          assetFileNames: (assetInfo) => {
-            // Картинки
-            if (/\.(png|jpe?g|gif|svg|webp|ico)$/i.test(assetInfo.name || "")) {
-              return "images/[name].[hash][extname]";
-            }
+        assetFileNames: (assetInfo) => {
+          // Картинки
+          if (/\.(png|jpe?g|gif|svg|webp|ico)$/i.test(assetInfo.name || "")) {
+            return "images/[name].[hash][extname]";
+          }
 
-            // Шрифты
-            if (/\.(woff2?|eot|ttf|otf)$/i.test(assetInfo.name || "")) {
-              return "fonts/[name].[hash][extname]";
-            }
+          // Шрифты
+          if (/\.(woff2?|eot|ttf|otf)$/i.test(assetInfo.name || "")) {
+            return "fonts/[name].[hash][extname]";
+          }
 
-            // Видео
-            if (/\.(mp4|webm|ogg)$/i.test(assetInfo.name || "")) {
-              return "videos/[name].[hash][extname]";
-            }
+          // Видео
+          if (/\.(mp4|webm|ogg)$/i.test(assetInfo.name || "")) {
+            return "videos/[name].[hash][extname]";
+          }
 
-            if (/\.css$/i.test(assetInfo.name || "")) {
-              return "styles/[name].[hash][extname]";
-            }
+          if (/\.css$/i.test(assetInfo.name || "")) {
+            return "styles/[name].[hash][extname]";
+          }
 
-            // Остальные ассеты (CSS, иконки и т.д.)
-            return "assets/[name].[hash][extname]";
-          },
+          // Остальные ассеты (CSS, иконки и т.д.)
+          return "assets/[name].[hash][extname]";
         },
       },
     },
-    plugins: [htmlInclude()]
-})
+  },
+  plugins: [htmlInclude()],
+});
