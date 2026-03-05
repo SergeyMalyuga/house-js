ab0af88062c2479a082d59829bc9c18ddaf6487e
Sergey Malyuga <sergey@malyuga.ru>
2026-03-05 12:49:22 +0300

    refactor(global): удалить header.scss и main.scss

--- src/components/header/Header.ts
+++ src/components/header/Header.ts
@@ -4,20 +4,20 @@ export class Header {
   public render() {
     return `
     <header class="${style.header}" role="banner">
-  <div class="header container">
-    <a class="link-log" href="/">
+  <div class="${style.content} container">
+    <a class="${style.linkLogo}" href="/">
       <img src="/images/vector/logo.svg" width="143" height="35" alt="Логотип компании 'House'">
     </a>
     <nav class="nav" aria-label="Основная навигация">
-      <ul class="list-nav">
+      <ul class="list">
         <li class="item-nav-link">
-          <a class="header-main__link-nav" href="#">Репродукции</a>
+          <a class="linkNav" href="#">Репродукции</a>
         </li>
         <li class="item-nav-link">
-          <a class="link-nav" href="#">Новинки</a>
+          <a class="linkNav" href="#">Новинки</a>
         </li>
         <li class="item-nav-link">
-          <a class="link-nav" href="#">О нас</a>
+          <a class="linkNav" href="#">О нас</a>
         </li>
       </ul>
     </nav>
