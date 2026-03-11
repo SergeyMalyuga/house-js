bc25e97e83570f1c7594909d83af0f37374bc580
Sergey Malyuga <sergey@malyuga.ru>
2026-03-11 10:54:12 +0300

    feat(Hero): создать блок с декоративной фотографией
    - <div class='blockImage'>
    - блок содержит картинки для разных разрешений:
    1440px 768px 576px

--- src/components/hero/Hero.ts
+++ src/components/hero/Hero.ts
@@ -1,8 +1,21 @@
+import styles from './Hero.module.scss';
+
 export class Hero {
   public render() {
     return `
-    <section class="hero">
-    <div class="content container">
+    <section>
+    <div class="${styles.content} container">
+    <div class="${styles.blockImage}">
+    <picture>
+    <source srcset="/images/raster/hero-bird-1440.png" media="(min-width: 769px)"/>
+    <source srcset="/images/raster/hero-bird-768.png" media="(min-width: 569px)"/>
+    <img src="/images/raster/hero-bird-576.png" alt=""/>
+</picture>
+</div>
+<div class="${styles.blockInfo}">
+<h1 class="${styles.title}">Реплики картин от <span>Ink. House</span></h1>
+<p class="${styles.text}">Высокое качество отрисовки на плотной бумаге или льняном холсте. Редкие произведения, доступные цены.</p>
+</div>
 </div>
 </section>`
   }
