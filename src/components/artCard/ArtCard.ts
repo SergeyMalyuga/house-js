9b2bad61e5a834d911fcce81952a8a17de70da89
Sergey Malyuga <sergey@malyuga.ru>
2026-03-16 10:00:52 +0300

    feat(Gallery.module): добавить адаптивность для списка картин
    - default: grid-template-columns: repeat(3, 1fr)
    - width <= 768px: grid-template-columns: repeat(2, 1fr)
    - width <= 480px: grid-template-columns: 1fr

--- src/components/artCard/ArtCard.ts
+++ src/components/artCard/ArtCard.ts
@@ -4,11 +4,12 @@ import type {Art} from '../../models/Art.model.ts';
 export class ArtCard {
   public render(card: Art): HTMLElement {
     const article = document.createElement('article');
+    article.className = `${styles.card}`;
     article.innerHTML = `
       <picture class="${styles.wrapperImage}">
       <source srcset="${card.image.mobile}" media="(max-width: 480px)"/>
-      <source srcset="${card.image.tabletMini}" media="(max-width: 768px)"/>
-      <source srcset="${card.image.tablet}" media="(max-width: 576px)"/>
+      <source srcset="${card.image.tablet}" media="(max-width: 768px)"/>
+      <source srcset="${card.image.tabletMini}" media="(max-width: 576px)"/>
       <img src="${card.image.desktop}">
   </picture>
   <span>${card.author}</span>
