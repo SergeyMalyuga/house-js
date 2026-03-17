e649bff1f5056c6ac06523d82ae811c755300327
Sergey Malyuga <sergey@malyuga.ru>
2026-03-17 10:41:04 +0300

    feat(CountryToggle.data): создать данные для рендеринга кнопок переключения страны

---
+++ src/data/countryToggle.data.ts
@@ -0,0 +1,17 @@
+import {Country} from '../constants/consts.ts';
+import type {CountryToggleModel} from '../models/CountryToggle.model.ts';
+
+export const getCountriesToggle = (): CountryToggleModel[] => [
+  {
+    text: 'Франция',
+    country: Country.FR,
+  },
+  {
+    text: 'Германия',
+    country: Country.DE,
+  },
+  {
+    text: 'Англия',
+    country: Country.GB,
+  }
+]
