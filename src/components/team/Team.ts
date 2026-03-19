ca06be17942a7e9fcb50383aa290d84e18045e91
Sergey Malyuga <sergey@malyuga.ru>
2026-03-19 10:00:20 +0300

    feat(Team): добавить декоративную картинку
    - .container -> .wrapperImage -> img
    - адаптировать:
                  - desktop: team-hand-desktop.png
                  - ≤768px: team-hand-tablet.png
                  - ≤576px: team-hand-tablet-mini.png
                  - ≤480px: team-hand-mobile.png

--- src/components/team/Team.ts
+++ src/components/team/Team.ts
@@ -1,9 +1,18 @@
 import {SectionID} from '../../constants/consts.ts';
+import styles from './Team.module.scss';
 
 export class Team {
   public render(): string {
     return `
-    <section id="${SectionID.TEAM}">
+    <section id="${SectionID.TEAM}" class="${styles.team}">
+    <div class="container">
+        <picture class="${styles.wrapperImage}">
+ <source srcset="/images/raster/team-hand-tablet.png" media="(max-width: 768px)">
+ <source srcset="/images/raster/team-hand-tablet-mini.png" media="(max-width: 576px)">
+ <source srcset="/images/raster/team-hand-mobile.png" media="(max-width: 480px)">
+ <img src="/images/raster/team-hand-desktop.png" alt="" loading="lazy">
+</picture>
+</div>
 </section>
     `
   }
